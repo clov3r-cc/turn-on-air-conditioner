23a0d7f1ec075411fdcc9601f8fc470300f9d9b8
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-07-29 10:25:27 +0900

    fix: fix discord datetime format

--- src/discord.ts
+++ src/discord.ts
@@ -1,13 +1,13 @@
-import { formatJstDateTime } from './date';
+import { formatDateTimeWithTz } from './date';
 import { HttpError } from './error';
 
-export const notifyAirConditionerOnToDiscord = async (webhookUrl: string, jstNow: Date, currentTemp: number) => {
+export const notifyAirConditionerOnToDiscord = async (webhookUrl: string, now: Date, currentTemp: number) => {
   const body = {
     embeds: [
       {
         title: 'エアコンをONにしました',
         description: '室温が指定の気温を上回ったことを確認したため、エアコンをONにしました。',
-        timestamp: formatJstDateTime(jstNow),
+        timestamp: formatDateTimeWithTz(now),
         color: 5620992,
         fields: [{ name: '現在の気温', value: `${currentTemp}℃`, inline: true }],
       },
