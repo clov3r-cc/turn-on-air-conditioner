984c19a698baf229accde8a2a953735aa5576955
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-07-31 21:22:42 +0900

    fix: fix datetime timezone

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,4 +1,4 @@
-import { formatDateTimeWithTz } from './date';
+import { formatInTimeZone } from 'date-fns-tz';
 import { HttpError } from './error';
 
 export const notifyAirConditionerOnToDiscord = async (webhookUrl: string, now: Date, currentTemp: number) => {
@@ -7,7 +7,7 @@ export const notifyAirConditionerOnToDiscord = async (webhookUrl: string, now: D
       {
         title: 'エアコンをONにしました',
         description: '室温が指定の気温を上回ったことを確認したため、エアコンをONにしました。',
-        timestamp: formatDateTimeWithTz(now),
+        timestamp: formatInTimeZone(now, 'UTC', 'yyyy-MM-dd HH:mm:ssXXX'),
         color: 5620992,
         fields: [{ name: '現在の気温', value: `${currentTemp}℃`, inline: true }],
       },
